44bef9dfd7d53cf56740007ecec726a467e09781
Artem Alagizov <artem.alagizov@asml.com>
2019-01-03 09:48:14 +0100

    added key pair and endpoint for public key retrieval

--- authentication-service/app/controllers/authenticate_controller.rb
+++ authentication-service/app/controllers/authenticate_controller.rb
@@ -1,47 +1,71 @@
 # frozen_string_literal: true
+
 require 'jwt'
 
 class AuthenticateController < ApplicationController
+  # POST at /authenticate [returns jwt if credentials in params are valid]
   def create
     result = get_result(params)
 
-    json_response({message: result}, result[:status])
+    json_response({ message: result }, result[:status])
   end
+
+  # GET at /authenticate [returns public key]
+  def index
+    json_response({ public_key: ENV['PUBLIC_KEY'] }, 200)
+  end
+
+  private
 
   def get_result(params)
-    login = params[:values][:"userName"]
-    password = params[:values][:"password"]
+    login = params[:values][:userName]
+    password = params[:values][:password]
     response = authenticate(login, password)
 
-    handle_response_status(response.status)
+    handle_response_status(response)
   end
 
-  def handle_response_status(status)
-    if status == 200
-      return {status: 200, token: generate_jwt_token}
-    end
+  def handle_response_status(response)
+    status = response[:status]
+    username = response[:username]
 
-    {status: 401}
+    return { status: 200, token: generate_jwt_token(username) } if status == 200
+
+    { status: 401 }
   end
 
-  def generate_jwt_token
-    payload = {data: 'test'}
-    rsa_private = OpenSSL::PKey::RSA.generate 2048
-    rsa_public = rsa_private.public_key
+  def generate_jwt_token(username)
+    JWT.encode get_payload(username),
+               OpenSSL::PKey::RSA.new(Base64.decode64(ENV['PRIVATE_KEY'])),
+               'RS256'
+  end
 
-    JWT.encode payload, rsa_private, 'RS256'
+  def get_payload(username)
+    exp = Time.now.to_i + 4 * 3600
+
+    {
+      username: username,
+      role: 'user role to be checked in a db to see access level',
+      exp: exp,
+      iss: 'auth microservice'
+    }
   end
 
   def authenticate(login, password)
-    data = {:sf_username => login, :sf_password => password}
+    data = { sf_username: login, sf_password: password }
     conn = Faraday.new(url: Settings.spotfire_server)
 
-    conn.post do |req|
+    response = conn.post do |req|
       req.url '/spotfire/sf_security_check'
       req.headers['Content-Type'] = 'application/x-www-form-urlencoded'
       req.headers['X-Requested-With'] = 'XMLHttpRequest'
       req.headers['Referer'] = "#{Settings.spotfire_server}/spotfire/login.html"
       req.body = URI.encode_www_form(data)
     end
+
+    {
+      status: response.status,
+      username: login
+    }
   end
 end
--- authentication-service/config/application.rb
+++ authentication-service/config/application.rb
@@ -1,17 +1,17 @@
 require_relative 'boot'
 
-require "rails"
+require 'rails'
 # Pick the frameworks you want:
-require "active_model/railtie"
-require "active_job/railtie"
+require 'active_model/railtie'
+require 'active_job/railtie'
 # require "active_record/railtie"
 # require "active_storage/engine"
-require "action_controller/railtie"
-require "action_mailer/railtie"
-require "action_view/railtie"
-require "action_cable/engine"
-require "sprockets/railtie"
-require "rails/test_unit/railtie"
+require 'action_controller/railtie'
+require 'action_mailer/railtie'
+require 'action_view/railtie'
+require 'action_cable/engine'
+require 'sprockets/railtie'
+require 'rails/test_unit/railtie'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -27,10 +27,20 @@ module AuthService
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.before_configuration do
+      env_file = File.join(Rails.root, 'config', 'local_env.yml')
+
+      if File.exist?(env_file)
+        YAML.safe_load(File.open(env_file)).each do |key, value|
+          ENV[key.to_s] = value
+        end
+      end
+    end
+
     config.middleware.insert_before 0, Rack::Cors do
       allow do
         origins '*'
-        resource '*', :headers => :any, :methods => [:get, :post, :options]
+        resource '*', headers: :any, methods: %i[get post options]
       end
     end
   end
