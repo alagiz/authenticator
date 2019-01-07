4f34df676eb402f29546d80048f475bc35455727
Artem Alagizov <artem.alagizov@asml.com>
2019-01-07 08:57:53 +0100

    added api gateway

---
+++ api-gateway/app/controllers/authenticate_controller.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+class AuthenticateController < ApplicationController
+  # POST at /authenticate - posts credentials to the authentication service.
+  # [returns jwt in the return message if successful]
+  def create
+    conn = Faraday.new(url: Settings.authentication_service)
+
+    response = conn.post do |req|
+      req.url '/authenticate'
+      req.headers['Content-Type'] = 'application/json'
+      req.body = params.to_json
+    end
+
+    status = response.status
+    token = response.body
+
+    json_response({ message: { token: token } }, status)
+  end
+
+  # GET at /authenticate - tries to get jwt from the authentication service.
+  # [returns public key if successful]
+  def index
+    conn = Faraday.new(url: Settings.authentication_service)
+
+    conn.get do |req|
+      req.url '/authenticate'
+    end
+  end
+end
---
+++ api-gateway/config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  match '*path', :controller => 'application', :action => 'handle_options_request', :via => :options
+  resources :authenticate, only: %i[index show create]
+end
